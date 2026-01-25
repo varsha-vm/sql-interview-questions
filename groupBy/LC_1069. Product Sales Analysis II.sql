ee2bbfc405eedd699fe67780c4dc6a6538b94ae7
Varsha Vaman Murthy <varshavamanmurthy636@gmail.com>
2026-01-25 11:58:15 -0800

    Adding solution to LC 1069

---
+++ groupBy/LC_1069. Product Sales Analysis II.sql
@@ -0,0 +1,7 @@
+-- Write your MySQL query statement below
+SELECT      
+        p.product_id,
+        SUM(quantity) AS total_quantity
+FROM sales s
+JOIN product p ON s.product_id = p.product_id
+GROUP BY p.product_id;
